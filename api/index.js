c7322e66e9d11b65b996352696a338fd2d9381e8
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2023-08-29 23:25:02 +0530

    create-post page

---
+++ api/index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+const Post = require("./models/Post");
+
+require("dotenv").config();
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "jlfbgkljabvakdvadvfafvdbgtg";
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(
+  "mongodb+srv://ashutoshpadhy001:zjephmXXXDOViZKH@cluster0.tri05hh.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  if (!userDoc) res.status(400).json("Wrong Credentials");
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw error();
+      res.cookie("token", token).json({ id: userDoc._id, username });
+    });
+  } else {
+    res.status(400).json("Wrong Credentials");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+  const { title, summary, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+  res.json(postDoc);
+});
+
+app.get("/post", async (req, res) => {
+  res.json(await Post.find());
+});
+
+app.listen(4000, () => {
+  console.log("server listening on 4000");
+});
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Post = () => {
+const Post = ({ title, summary, content, cover }) => {
   return (
     <div className="post">
       <img
@@ -9,26 +9,14 @@ const Post = () => {
         className="image"
       />
       <div className="texts">
-        <h2>
-          Tesla’s China rival Xpeng buys ride hailing giant Didi’s smart EV
-          assets for $744M
-        </h2>
+        <h2>{title}</h2>
         <p className="info">
           <a className="author" href="/">
             Ashutosh Padhy
           </a>
           <time>28-03-2023 11:40</time>
         </p>
-        <p className="summary">
-          Chinese electric vehicle upstart Xpeng is acquiring the smart EV
-          assets of ride hailing giant Didi for $744 million, marking another
-          significant alliance that the Tesla challenger has struck in recent
-          months. In an announcement on Monday, Didi said the duo is forming a
-          strategic partnership to “promote the global application of smart
-          electric vehicles and technologies.” Notably, the Didi assets will
-          become a new sub-brand called “Mona” under Xpeng, which is scheduled
-          to launch in 2024.
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
