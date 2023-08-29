21cf4cee205c9a6f6cb708ca639d2e21fec97583
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2023-08-29 22:40:13 +0530

    added create post page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import { Navigate } from "react-router-dom";
+import "react-quill/dist/quill.snow.css";
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
 
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+    console.log(data);
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
-    <form>
-      <input type="title" placeholder="title" />
-      <input type="summary" placeholder="summary" />
-      <input type="file" />
-      {/* <textarea name="" rows="10" cols="30" id=""></textarea> */}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="title"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder="summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        modules={modules}
+        formats={formats}
+        value={content}
+        onChange={(newVal) => setContent(newVal)}
+      />
+      <button
+        style={{ marginTop: "5px" }}
+        className="create"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      >
+        Create Post
+      </button>
     </form>
   );
 };
